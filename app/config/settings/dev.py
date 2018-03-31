717cd86159d54c23b1cae18e073ef4072168b0c3
himanmen Git <study.himanmen@gmail.com>
2018-04-01 04:47:04 +0900

    ec2 에서 static 서빙

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -16,4 +16,4 @@ INSTALLED_APPS += [
 ]
 
 DEFAULT_FILE_STORAGE = 'config.storage.DefaultFilesStorage'
-STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
