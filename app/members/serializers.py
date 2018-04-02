2994e282f786e7c32c31b4017da662dade97095f
smallbee <smallbee3@gmail.com>
2018-04-03 08:59:42 +0900

    DELETE /user/<pk>/ available

--- app/members/serializers.py
+++ app/members/serializers.py
@@ -10,6 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
+            'id',
             'username',
             'email',
             'first_name',
