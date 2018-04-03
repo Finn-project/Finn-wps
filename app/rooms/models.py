372d1e1859ffb5ece7fabe02b7b23bd9faf41eb6
himanmen Git <study.himanmen@gmail.com>
2018-04-03 22:38:47 +0900

    rooms 모델 작업중

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 
 class Room(models.Model):
-    pass
+    ROOM_TYPE_APARTMENT = 'AP'
+    ROOM_TYPE_HOUSING = 'HO'
+    ROOM_TYPE_ONEROOM = 'OR'
+
+    ROOM_TYPE_CHOICES = (
+        (ROOM_TYPE_APARTMENT, '아파트'),
+        (ROOM_TYPE_HOUSING, '주택'),
+        (ROOM_TYPE_ONEROOM, '원룸'),
+    )
+
+    room_type = models.CharField(max_length=2, choices=ROOM_TYPE_CHOICES, default=ROOM_TYPE_HOUSING)
+    personnel = models.SmallIntegerField()
