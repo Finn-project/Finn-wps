0ba14a1fa27ed0fb3a1237a8bf4b6166769e8dbe
smallbee <smallbee3@gmail.com>
2018-04-09 09:19:31 +0900

    User Update API test added (Request & Response done, verification of response needed)

--- app/members/tests.py
+++ app/members/tests.py
@@ -2,6 +2,7 @@ import random
 
 from django.contrib.auth import get_user_model, authenticate
 from django.test import TestCase
+from django.test.client import encode_multipart
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
@@ -166,6 +167,57 @@ class UserDeleteTest(APITestCase):
 
 
 class UserUpdateTest(APITestCase):
+    def setUp(self):
+        # test user 생성
+        test_user_info = {
+            'username': 'test_user_01@gmail.com',
+            'password': 'asdfqwer',
+            'confirm_password': 'asdfqwer',
+            'first_name': 'Park',
+            'last_name': 'Boyoung',
+            'phone_num': '010-1234-5678',
+        }
+        self.client.post('/user/', test_user_info)
 
     def test_user_update(self):
-        pass
+        """
+        Method: POST
+        일반회원이 업데이트 테스트
+        :return:
+        """
+
+        # 미리 생성된 test user로 로그인 후 Header에 Token 값 넣기
+        test_user_info = {'username': 'test_user_01@gmail.com', 'password': 'asdfqwer'}
+        response = self.client.post('/user/login/', test_user_info)
+        token = response.json()['token']
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + token,
+        )
+
+        # User Update 요청 부분
+
+        img_profile = open('../.static/iu.jpg', 'rb')
+
+        request_contents = {
+            'email': 'test_user_02@gmail.com',
+            'password': 'asdfqwer1',
+            'confirm_password': 'asdfqwer1',
+            'first_name': '이유',
+            'last_name': '아',
+            'phone_num': '011-1111-1111',
+            'img_profile': img_profile,
+        }
+        encoded_contents = encode_multipart('BoUnDaRyStRiNg', request_contents)
+        content_type = 'multipart/form-data; boundary=BoUnDaRyStRiNg'
+
+        user = User.objects.get(username='test_user_01@gmail.com')
+        response = self.client.put(
+            f'/user/{user.pk}/',
+            encoded_contents,
+            content_type=content_type,
+        )
+        img_profile.close()
+
+        result = response.json()
+
+        print(result)
