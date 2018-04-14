5ad282f593578bfda540822848e7b059b458600e
smallbee <smallbee3@gmail.com>
2018-04-14 15:24:24 +0900

    members.serializer Refactoring : Separate modules

--- app/members/serializers/user.py
+++ app/members/serializers/user.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from members.serializers import UserProfileImagesSerializer
+from ..serializers import UserProfileImagesSerializer
 
 User = get_user_model()
 
