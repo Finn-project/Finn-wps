1f9320cfa52941124d49e4f2e562e7d3580523fa
smallbee <smallbee3@gmail.com>
2018-04-22 19:51:26 +0900

    Airbnb selenium crawler updated: house crawler separated (page / detail)

--- app/utils/crawler/airbnb_selenium.py
+++ app/utils/crawler/airbnb_selenium.py
@@ -1,13 +1,19 @@
+import json
 import os
 import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-# import django
-# django.setup()
-
 # 한줄로 처리
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+
+import django
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class AirbnbCrawler:
@@ -25,16 +31,11 @@ class AirbnbCrawler:
         html = self.driver.page_source
         soup = BeautifulSoup(html, 'lxml')
 
-        # if soup.select_one('span._13nd2f7d > h1._1xu9tpch') is None:
-        #     with open('.../test.html', 'wt') as f:
-        #         f.write(html)
-        #     print('test.html에 기록하고 종료')
-        #     return
-        with open('test.html', 'wt', encoding='utf8') as f:
-            f.write(html)
+        # (1) Regulation + BeautifulSoup 이용한 crawling
 
         # 1) name
         name = soup.select_one('span._13nd2f7d > h1._1xu9tpch').get_text(strip=True)
+        print(name)
 
         # 2) description
         span_list = soup.select_one('p._6z3til').select('span')
@@ -51,15 +52,15 @@ class AirbnbCrawler:
                 break
                 # '정확한 위치는 예약 완료 후에 표시됩니다'를 제외하기 위한 예외처리
         description = ''.join(description_list)
+        print(description)
 
-        # 3) room, bed, bathroom, personnel
+        # 3) 4) 5) 6) room, bed, bathroom, personnel
         span_list = soup.select('div._1thk0tsb > span._y8ard79')
-        print(span_list)
         basic_info_list = []
         for span in span_list:
             text = span.get_text(strip=True)
             basic_info_list.append(text)
-        print(basic_info_list)
+        # print(basic_info_list)
 
         def find_num(string):
             result = re.search(r'.*?(\d).*?', string)
@@ -74,48 +75,162 @@ class AirbnbCrawler:
         bed = find_num(basic_info_list[2])
         bathroom = find_num(basic_info_list[3])
         personnel = find_num(basic_info_list[0])
+        # print(personnel)
+        # print(room)
+        # print(bed)
+        # print(bathroom)
 
-        print(personnel)
-        print(room)
-        print(bed)
-        print(bathroom)
+        # 7) price_per_night
+        price_str = soup.select_one('div._12cv0xt > div > div._36rlri > span._10cqp947 > span').text
+        # print(price_str)
+        price_per_night = int(re.sub(',', '', re.sub('₩', '', price_str)))
 
-        soup = BeautifulSoup(html, 'lxml')
-
-        #                       neighborhood > div > div > div > div._e296pg > section > div._e296pg > div > div._1fmyluo4 > div > div > div > div:nth-child(9) > div:nth-child(2) > a
-        #                       neighborhood > div > div > div > div._e296pg > section > div._e296pg > div > div._1fmyluo4 > div > div > div > div:nth-child(2) > a
-        href = soup.select_one('div#neighborhood > div > div > div > div._e296pg > section > div._e296pg > div > div._1fmyluo4')
-        # href = soup.select_one('dev._1fmyluo4 > div > div > div > div:nth-child(2) > a')
-        # href = soup.select_one('dev._1fmyluo4 > div > div > div > div:nth-child(2) > a').get('href')
-        print(href)
+        # 8) lat, lng (Selenium으로도 brower 조회 데이터를 불러오지 못해 실패)
+        #                             neighborhood > div > div > div > div._e296pg > section > div._e296pg > div > div._1fmyluo4 > div > div > div > div:nth-child(9) > div:nth-child(2) > a
+        #                             neighborhood > div > div > div > div._e296pg > section > div._e296pg > div > div._1fmyluo4 > div > div > div > div:nth-child(2) > a
+        # href = soup.select_one('div#neighborhood > div > div > div > div._e296pg > section > div._e296pg > div > div._1fmyluo4')
+        # print(href)
         # lat_lng = re.search('.*?=(.*?),(.*?)=', href)
         # lat = lat_lng.group(1)
         # lng = lat_lng.group(2)
-        # print('lat')
-        # print('lng')
 
-        minimum_check_in_duration = 1,
-        maximum_check_in_duration = 3,
-        maximum_check_in_range = 90,
-        price_per_night = 100000,
-        country = ''
-        city = ''
-        district = ''
-        dong = ''
-        address1 = ''
-        latitude = ''
-        longitude = ''
+        # (2) json 내부 데이터 parsing 활용
+
+        bootstrap_data = re.search(r'data-hypernova-key="spaspabundlejs" data-hypernova-id=".*?">&lt;!--(.*?)--&gt;</script>', html)
+        # print(bootstrap_data.group(1))
+        bootstrap_json = json.loads(bootstrap_data.group(1))
+        # print(bootstrap_json)
+        listing_dict = \
+            bootstrap_json['bootstrapData']['reduxData']['homePDP']['listingInfo']['listing']
+
+        # # house_info
+
+        # 1) name (BeutifulSoup crawling 데이터로 사용)
+        # name = listing_dict['p3_subject']
+        # print(name)
+
+        # 8) minimum_check_in_duration = 1,
+        minimum_check_in_duration = listing_dict['min_nights']
+        print(minimum_check_in_duration)
+
+        # 9) 10) 11) 12) 13)
+        address_list = listing_dict['location_title'].split(', ')
+        print(address_list)
+        length = len(address_list)
+        # 9) country
+        country = address_list[length-1]
+        print(country)
+        # 10) city
+        city = address_list[length-2]
+        print(city)
+        # 11) district
+        district = listing_dict['localized_city']
+        # district = address_list[length-3]
+        print(district)
+        # 12) dong = ''
+        # 13) address1
+        address1 = address_list[0] if length > 3 else ''
+        # 주소 명칭이 4개 있을 경우 가장 첫번째 단어를 상세주소(address1)에 할당
+        print(address1)
+
+        # 14) latitude
+        lat = listing_dict['lat']
+        print(lat)
+
+        # 15) longitude
+        lng = listing_dict['lng']
+        print(lng)
+
+        # 16) img_cover
+        img_cover_url = listing_dict['photos'][0]['large']
+        print(img_cover_url)
+
+        # 17) inner
+        # inner images 존재 x 경우 예외처리(1)
+        if len(listing_dict['photos']) > 1:
+            inner_img_url_1 = listing_dict['photos'][1]['large']
+            print(inner_img_url_1)
+        else:
+            inner_img_url_1 = ''
+
+        # inner images 존재 x 경우 예외처리(2)
+        if len(listing_dict['photos']) > 2:
+            inner_img_url_2 = listing_dict['photos'][2]['large']
+            print(inner_img_url_2)
+        else:
+            inner_img_url_2 = ''
+
+        # 18)
         # disable_days = [
         #     '2014-01-01',
         #     '2014-02-01',
         #     '2014-03-01',
         #     '2014-04-01',
         # ],
-        # amenities = [],
-        # facilities = [1, 2, 3, 4, 5],
 
-        result = {}
-        return result
+        # 19) amenities = [],
+
+        # 20) facilities = [1, 2, 3, 4, 5],
+
+        # 21) maximum_check_in_duration = 3,
+        # 22) maximum_check_in_range = 90,
+        # 23) personnel
+        # personnel = listing_dict['person_capacity']
+        # print(personnel)
+
+        house_data = {
+            'house_id': house_id,
+            # 'house_type': 'HO',
+            'name': name,
+            'description': description,
+            'room': room,
+            'bed': bed,
+            'bathroom': bathroom,
+            'personnel': personnel,
+            # 'amenities': [],
+            # 'facilities': [1, 2, 3, 4, 5],
+            'minimum_check_in_duration': minimum_check_in_duration,
+            'maximum_check_in_duration': 5,
+            'maximum_check_in_range': 90,
+            'price_per_night': price_per_night,
+            'country': country,
+            'city': city,
+            'district': district,
+            # 'dong': 'default',
+            'address1': address1,
+            'lat': lat,
+            'lng': lng,
+            # 'disable_days': [
+            #     '2014-01-01',
+            #     '2014-02-01',
+            #     '2014-03-01',
+            #     '2014-04-01',
+            # ],
+            'img_cover_url': img_cover_url,
+            'inner_img_url_1': inner_img_url_1,
+            'inner_img_url_2': inner_img_url_2,
+        }
+
+        # # host_user info
+
+        # 1) username
+        username = str(listing_dict['user']['id']) + '@finn.com'
+        print(username)
+
+        # 2) first_name
+        first_name = username
+        print(first_name)
+
+        # 3) img_profile path
+        img_profile_url = listing_dict['user']['profile_pic_path']
+        print(img_profile_url)
+
+        host_user_data = {
+            'username': username,
+            'password': username,
+            'first_name': first_name,
+            'img_profile_url': img_profile_url,
+        }
 
     def house_page_crawling(self):
 
@@ -141,26 +256,24 @@ class AirbnbCrawler:
 
         for city in city_list:
 
-            print('---------------------------------------------')
+            print('---------------------------------------------------------------')
             print(f'{city} 지역의 숙소를 크롤링 시작')
-            print('---------------------------------------------')
+            print('---------------------------------------------------------------')
 
             for num in range(num_of_pages):
 
-                print('------------------------------------------------')
+                print('---------------------------------------------------------------')
                 print(f'{city} 지역의 숙소 검색결과 목록 중 "{num+1}" 페이지를 크롤링 중입니다..')
-                print('------------------------------------------------')
+                print('---------------------------------------------------------------')
 
                 url = f'https://www.airbnb.co.kr/s/homes?query={city}&section_offset={num+1}'
-                # response = requests.get(url)
-
-                # with open('../test.html', 'wt') as f:
-                #     f.write(html)
-                # -> 동적으로 페이지가 구성되기 때문에 Selenium으로 불러와야함.
 
                 self.driver.get(url)
                 html = self.driver.page_source
                 soup = BeautifulSoup(html, 'lxml')
+
+                # with open('test1.html', 'wt', encoding='utf8') as f:
+                #     f.write(html)
 
                 house_info = {}
                 itemListElement_list = soup.find('div', class_='_fhph4u').find_all('div', class_='_gig1e7')
@@ -170,13 +283,12 @@ class AirbnbCrawler:
                     house_id = house_id_str.split('-')[1]
 
                     print(f'숙소 {house_id}를 크롤링합니다.')
-                    result = self.house_detail_crawling(house_id)
-                    house_info[id] = result
+                    self.house_detail_crawling(house_id)
                     break
 
 
 if __name__ == '__main__':
     air = AirbnbCrawler(3)
     # air.house_page_crawling()
-    air.house_detail_crawling(17563112)
-    # air.house_detail_crawling(19350356)
+    # air.house_detail_crawling(17563112)
+    air.house_detail_crawling(19350356)
