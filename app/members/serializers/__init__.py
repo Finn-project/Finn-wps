30cd80b41e664afe444fe8d981e8e5d9d8dc4fa7
smallbee <smallbee3@gmail.com>
2018-04-23 17:26:52 +0900

    Import error fixed: 'from ..serializers import ~' -> 'from ..serializers.user_image import~'

--- app/members/serializers/__init__.py
+++ app/members/serializers/__init__.py
@@ -1,11 +1,16 @@
 from .facebook import *
 from .facebook_auth import *
-from .user_image import *
+
 from .user import *
+from .user_image import *
 from .user_create import *
 from .user_update import *
+
+
+
 
 
 # 여러번 테스트 결과 __init__.py에서 import 하는 모듈이 import하려는
 # 모듈보다 위에있어서 에러가 난 것으로 추측.
 # 정확한 이유는 불명
+
