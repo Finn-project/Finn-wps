5d347752ef5a4c0751bf38810fe5d864f0f6f4a5
smallbee <smallbee3@gmail.com>
2018-05-02 23:31:04 +0900

    reservation.tests turns to package, tests.create.py added

--- app/reservation/tests/create.py
+++ app/reservation/tests/create.py
@@ -1,6 +1,9 @@
 import os
+from datetime import timedelta
 
 from django.conf import settings
+from django.utils import timezone
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
 
 from utils.image.resize import clear_imagekit_test_files
@@ -101,29 +104,39 @@ class ReservationAPITest(APITestCase):
         }
 
         response = self.client.post(self.URL1, data)
-        print(response)
-        # print(response.data.get('serializer').instance)
-        print(response.data.get('pk'))
         house_pk = response.data.get('pk')
-
         house = get_object_or_404(House, pk=house_pk)
-        print(house)
 
-        # # Host User 숙소 등록
-        # self.house = House.objects.create(
-        # )
+        # # Reservation 생성
 
-        # Reservation 생성
+        # 1) guest의 token값 client에 넣기
         self.client.credentials(
             HTTP_AUTHORIZATION='Token ' + self.token.key,
         )
+
+        # 2) 예약 관련 데이터 세팅
+        now = timezone.now()
+        check_in_date = (now + timedelta(1)).strftime('%Y-%m-%d')
+        check_out_date = (now + timedelta(2)).strftime('%Y-%m-%d')
         data = {
-            'check_in_date': '2018-04-25',
-            'check_out_date': '2018-04-27',
-            'house': '1',
+            'check_in_date': check_in_date,
+            'check_out_date': check_out_date,
+            'house': 1,
+            'guest_num': 3,
         }
+
+        # 3) post 요청으로 예약 생성
         response = self.client.post(self.URL2, data)
-        print(response)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(response.data['check_in_date'], data['check_in_date'])
+        self.assertEqual(response.data['check_out_date'], data['check_out_date'])
+        self.assertEqual(response.data['guest_num'], 3)
+        self.assertEqual(response.data['reservation_status'], 'RE')
+        self.assertEqual(response.data['reservation_current_state'], 'BE')
+        self.assertEqual(response.data['created_date'], timezone.now().strftime('%Y-%m-%d'))
+        self.assertEqual(response.data['modified_date'], timezone.now().strftime('%Y-%m-%d'))
 
         img_cover.close()
         house_image1.close()
